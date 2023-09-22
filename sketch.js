93d4b301ad14bcec5b5a67cf6e935d7b8201c36f
Fuguo Xue <fx2100@nyu.edu>
2023-09-22 14:45:59 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,24 @@
+let r1 = 15;
+let r2 = 5;
+let step = 30;
+let w = 600;
+let h = 800;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(238,218,1);
 }
 
 function draw() {
+  fill(0);
+  for (i=0; i<=windowWidth; i++){
+    for (j=0; j<=windowHeight; j++){
+      if (((i%2 == 0) && (j%2 == 0))||((i%2 == 1) && (j%2 == 1))){
+        ellipse(i*step, j*step, r1);
+      }
+      else {
+        ellipse(i*step, j*step, r2);
+      }
+    }
+  }
 }
